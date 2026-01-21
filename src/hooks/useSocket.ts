d7590782d7512d7df900b665c3f823e1ada72c88
elsawy1010 <48021396+elsawy1010@users.noreply.github.com>
2026-01-21 11:21:52 +0200

    Update useSocket.ts

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import { io, Socket } from 'socket.io-client'
 
-const SOCKET_URL = typeof window !== 'undefined' 
-  ? `${window.location.protocol}//${window.location.hostname}:3001`
-  : 'http://localhost:3001'
+const SOCKET_URL = typeof window !== 'undefined'
+    ? `${window.location.protocol}//${window.location.host}`
+    : 'http://localhost:3001'
 
 export function useSocket() {
     const [socket, setSocket] = useState<Socket | null>(null)
-    
+
     function connect() {
         if (!socket) {
             const newSocket = io(SOCKET_URL)
